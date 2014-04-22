2579422b1d9b9525269612b2ef5fb6d212040ec7
Darren Cooney <dcooney@ecentricarts.com>
2014-04-22 10:38:07 -0400

    template fix

--- template-ajax-load-more.php
+++ template-ajax-load-more.php
@@ -8,7 +8,7 @@ Template Name: Ajax Load More
 	<h3>A simple solution for lazy loading WordPress posts</h3>
 	<!-- AJAX LOAD MORE -->
 	<section id="ajax-load-more">
-		<ul class="listing" data-path="<?php echo get_template_directory_uri(); ?>/ajax-load-more" data-post-type="post" data-category="" data-taxonomy="" data-tag="" data-search="" data-display-posts="2" data-scroll="true" data-max-pages="5" data-button-text="<?php echo _e('Older Posts', 'framework'); ?>" data-transition="slide">
+		<ul class="listing" data-path="<?php echo get_template_directory_uri(); ?>/ajax-load-more" data-post-type="post" data-category="" data-taxonomy="" data-tag="" data-search="" data-display-posts="2" data-scroll="true" data-max-pages="5" data-button-text="<?php _e('Older Posts', 'framework'); ?>" data-transition="slide">
 		<!-- Load Ajax Posts Here -->
 		</ul>
 	</section>
